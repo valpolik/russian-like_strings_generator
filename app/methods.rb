fe717a033011827073b8f28576ed762f3c2a7c02
Valentin Polikarpov <valpolik@mail.ru>
2021-11-24 11:50:50 +0300

    Добавлены вероятностные константы для реализации актуального распределения букв

--- app/methods.rb
+++ app/methods.rb
@@ -1,3 +1,76 @@
+LETTERS_FREQ = {
+  1072 => 801,  # а
+  1073 => 159,  # б
+  1074 => 454,  # в
+  1075 => 170,  # г
+  1076 => 298,  # д
+  1077 => 749,  # е
+  1105 => 100,  # ё
+  1078 => 94,   # ж
+  1079 => 165,  # з
+  1080 => 735,  # и
+  1081 => 121,  # й
+  1082 => 349,  # к
+  1083 => 440,  # л
+  1084 => 321,  # м
+  1085 => 670,  # н
+  1086 => 1097, # о
+  1087 => 281,  # п
+  1088 => 473,  # р
+  1089 => 547,  # с
+  1090 => 626,  # т
+  1091 => 262,  # у
+  1092 => 26,   # ф
+  1093 => 97,   # х
+  1094 => 48,   # ц
+  1095 => 144,  # ч
+  1096 => 73,   # ш
+  1097 => 36,   # щ
+  1098 => 4,    # ъ
+  1099 => 190,  # ы
+  1100 => 174,  # ь
+  1101 => 32,   # э
+  1102 => 64,   # ю
+  1103 => 201   # я
+}.freeze
+
+
+ONE_LETTER_WORDS_FREQ = {
+  1080 => 358, # аявоуикс. Удельный вес букв.
+  1074 => 314,
+  1103 => 127,
+  1089 => 113,
+  1072 => 82,
+  1082 => 54,
+  1091 => 43,
+  1086 => 34
+}.freeze
+
+
+def provide_distribution(hash)
+  sample_array = []
+  hash.each_key do |k|
+    hash[k].times do
+      sample_array << k
+    end
+  end
+  sample_array.freeze
+  sample_array
+end
+
+
+ONE_LETTER_WORDS_PROBABILITY_ARRAY = provide_distribution(ONE_LETTER_WORDS_FREQ)
+
+
+LETTERS_PROBABILITY_ARRAY          = provide_distribution(LETTERS_FREQ)
+
+
+
+
+
+
+
+
 def rl_str_gen    # Russian-like string generator
   base_string
 end
@@ -84,11 +157,20 @@ def make_common_word(hash)
   if hash[:multi_syllable]
     word = generate_multi_syllable_word
   elsif hash[:one_letter]
-    word = [1072, 1103, 1074, 1086, 1091, 1080, 1082, 1089].sample
+    word = ONE_LETTER_WORDS_PROBABILITY_ARRAY.sample
   else
     word = generate_single_syllable_word
   end
 
   word = add_dash(word) if hash[:dash]
   word
+end
+
+
+def generate_single_syllable_word
+  length = rand(20) < 15 ? rand(2..4) : rand(5..6)
+  vowel = [1072, 1086, 1091, 1101, 1099, 1080, 1103, 1077, 1105, 1102].sample # vowels
+
+  [1073, 1074, 1075, 1076, 1078, 1079, 1082, 1083, 1084, 1085, 1087, 1088, 1089, 1090, 1092, 1093, 1094, 1095, 1096, 1097] # consonants without ЙЬЪ
+
 end
